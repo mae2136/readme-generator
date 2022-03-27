4fe70ad1b7d10ba523e8f6b2f2641ac6357592ac
Miguel Escobar <mae2136@gmail.com>
2022-03-27 00:06:46 -0500

    V. 0.2 - Add initial inquirer user selection functionality.

--- index.js
+++ index.js
@@ -4,18 +4,77 @@ const Licenses = require("github-licenses");
 const licenseList = ["AFL", "APACHE", "MIT", "OSL", "ECL", "EUPL", "EPL2", "ART", "MSPL", "MPL"];
 
 // Ask user for typical readme information
-// Name
-// Github Name
-// Email
-// Project Name
-// Project Description
-// How do you install your program?
-// How do you use your program?
-// How can someone contribute to your program?
-// How can someone test your program?
-// Which license would you like to use? (See if npm for licenses exists)
+inquirer
+    .prompt([
+        {
+            // Name
+            type: 'input',
+            message: 'What is your name?',
+            name: 'name',
+        },
+        {
+            // Github Name
+            type: 'input',
+            message: 'What is your Github name?',
+            name: 'username',
+        },
+        {
+            // Email
+            type: 'input',
+            message: 'What is your email?',
+            name: 'email',
+        },
+        {
+            // Project Name
+            type: 'input',
+            message: 'What is your Project Name?',
+            name: 'projectName',
+        },
+        {
+            // Project Description
+            type: 'input',
+            message: 'Describe your project:',
+            name: 'projectDesc',
+        },
+        {
+            // How do you use your program?
+            type: 'input',
+            message: 'How do you use your program?',
+            name: 'usage',
+        },
+        {
+            // How do you install your program?
+            type: 'input',
+            message: 'How do you use your program?',
+            name: 'usage',
+        },
+        {
+            // How can someone contribute to your program?
+            type: 'input',
+            message: 'How do you use your program?',
+            name: 'contribute',
+        },
+        {
+            // How can someone test your program?
+            type: 'input',
+            message: 'How can someone test your program?',
+            name: 'test',
+        },
+        {
+            // Which license would you like to use?
+            type: 'list',
+            message: 'Which license do you want to use? (Pick one)',
+            name: 'license',
+            choices: licenseList,
+        },
+    ])
+    .then((response) => {
+        console.log(response);
+        console.log(`Success!`);
+    });
+
 // Add license badge (see if npm exists for this)
 
 
 // Generates license based on user choice
-// const licenses = new Licenses(`${}`, 2022)
+// const licenses = new Licenses(`${name}`, 2022)
