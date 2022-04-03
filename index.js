5717b8740e2a836f1bffc26019c57e76ddd3737a
Miguel Escobar <mae2136@gmail.com>
2022-04-02 22:34:58 -0500

    V. 0.6 - Added starter code provided by homework assignment.
    - Integrated old template in to starter code.
    
    - Start license set up.

--- index.js
+++ index.js
@@ -1,143 +1,98 @@
+// TODO: Include packages needed for this application
 const fs = require(`fs`);
 const inquirer = require(`inquirer`);
-const Licenses = require("github-licenses");
-const { generateKey } = require("crypto");
-const licenseList = ["AFL", "APACHE", "MIT", "OSL", "ECL", "EUPL", "EPL2", "ART", "MSPL", "MPL"];
+const markdown = require(`./utils/generateMarkdown`);
+const licenseList = ["AFL", "APACHE", "MIT", "OSL", "ECL", "EUPL", "EPL2", "ART", "MSPL", "MPL", "None"];
 
-// Ask user for typical readme information
-inquirer
-    .prompt([
-        {
-            // Name
-            type: 'input',
-            message: 'What is your name?',
-            name: 'name',
-        },
-        {
-            // Github Name
-            type: 'input',
-            message: 'What is your Github name?',
-            name: 'username',
-        },
-        {
-            // Email
-            type: 'input',
-            message: 'What is your email?',
-            name: 'email',
-        },
-        {
-            // Project Name
-            type: 'input',
-            message: 'What is your Project Name?',
-            name: 'project',
-        },
-        {
-            // Project Description
-            type: 'input',
-            message: 'Describe your project:',
-            name: 'description',
-        },
-        {
-            // How do you use your program?
-            type: 'input',
-            message: 'How do you use your program?',
-            name: 'usage',
-        },
-        {
-            // How do you install your program?
-            type: 'input',
-            message: 'How do you install your program?',
-            name: 'install',
-        },
-        {
-            // How can someone contribute to your program?
-            type: 'input',
-            message: 'How can someone contribute to your program?',
-            name: 'contribute',
-        },
-        {
-            // How can someone test your program?
-            type: 'input',
-            message: 'How can someone test your program?',
-            name: 'test',
-        },
-        {
-            // Which license would you like to use?
-            type: 'list',
-            message: 'Which license do you want to use? (Pick one)',
-            name: 'license',
-            choices: licenseList,
-        },
-        {
-            // Credit
-            type: 'input',
-            message: 'Would you like to credit anybody or organization?',
-            name: 'credit',
-        },
-    ])
-    .then((response) => {
-        console.log(`Success!`);
-        generateReadme(response);
-    });
+// TODO: Create a function to initialize app
+function init() {
+    // Ask user for typical readme information
+    inquirer
+        .prompt([
+            {
+                // Name
+                type: 'input',
+                message: 'What is your name?',
+                name: 'name',
+            },
+            {
+                // Github Name
+                type: 'input',
+                message: 'What is your Github name?',
+                name: 'username',
+            },
+            {
+                // Email
+                type: 'input',
+                message: 'What is your email?',
+                name: 'email',
+            },
+            {
+                // Project Name
+                type: 'input',
+                message: 'What is your Project Name?',
+                name: 'project',
+            },
+            {
+                // Project Description
+                type: 'input',
+                message: 'Describe your project:',
+                name: 'description',
+            },
+            {
+                // How do you use your program?
+                type: 'input',
+                message: 'How do you use your program?',
+                name: 'usage',
+            },
+            {
+                // How do you install your program?
+                type: 'input',
+                message: 'How do you install your program?',
+                name: 'install',
+            },
+            {
+                // How can someone contribute to your program?
+                type: 'input',
+                message: 'How can someone contribute to your program?',
+                name: 'contribute',
+            },
+            {
+                // How can someone test your program?
+                type: 'input',
+                message: 'How can someone test your program?',
+                name: 'test',
+            },
+            {
+                // Which license would you like to use?
+                type: 'list',
+                message: 'Which license do you want to use? (Pick one)',
+                name: 'license',
+                choices: licenseList,
+            },
+            {
+                // Credit
+                type: 'input',
+                message: 'Would you like to credit anybody or organization?',
+                name: 'credit',
+            },
+        ])
+        .then((response) => {
+            const readme = markdown.generateMarkdown(response);
+            writeToFile(readme);
+            console.log(`Success!`);
+        });
+}
 
 // Add license badge (see if npm exists for this)
 
-
-// Generates license based on user choice
-function generateReadme(response) {
-    console.log(response);
-    const {name, username, email, project, description, usage, install, contribute, test, license, credit} = response;
-    const licenses = new Licenses(`${name}, ${username}`, 2022);
-    let userlicense = licenses.license
-    let template = `# ${project}
-    ## Description
-    
-    This program was created by ${name}. If you have any questions on how this program works, please reach out to me at ${email}.
-
-    ${description}
-    
-    ## Table of Contents
-    
-    - [Installation] (#installation)
-    - [Usage] (#usage)
-    - [Credits] (#credits)
-    - [Contribution] (#contribution)
-    - [License] (#license)
-    
-    ## Installation
-    
-    Installation Instructions: ${install}
-    
-    ## Usage
-    This repository can be viewed at the following link: (link)
-    
-    It should look like the following:
-    
-    ![Screenshot](./screenshot.PNG)
-    
-    How to use the program:
-    \`\`\`
-    ${usage}
-    \`\`\`
-        
-    ## How to Contribute
-    
-    ${contribute}
-    
-    ## Tests
-    
-    ${test}
-
-    ## Credits
-    
-    ${credit}
-    
-    ## License
-    
-    ${userlicense}`;
-    
-    fs.writeFile(`readme.md`, template, (err) =>
-    err ? console.error(err) : console.log(`Readme file created!`)
+// TODO: Create a function to write README file
+function writeToFile(data) {
+    fs.writeFile(`README.md`, data, (err) =>
+        err ? console.error(err) : console.log(`README file created!`)
     );
 }
 // const licenses = new Licenses(`${name}`, 2022)
+
+// Function call to initialize app
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,74 @@
+const Licenses = require("github-licenses");
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license, name) {
+  if (license == "None") {
+    return "";
+  } else {
+    const licenses = new Licenses(`${name}`, 2022);
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  const {name, username, email, project, description, usage, install, contribute, test, license, credit} = data;
+  licenseTemplate = renderLicenseSection(license, name);
+  let userlicense = licenses.license
+  return `# ${project}
+  ## Description
+  
+  This program was created by ${username}. If you have any questions on how this program works, please reach out to me at ${email}.
+
+  ${description}
+  
+  ## Table of Contents
+  
+  - [Description] (#description)
+  - [Installation] (#installation)
+  - [Usage] (#usage)
+  - [Credits] (#credits)
+  - [Contributing] (#contributing)
+  - [License] (#license)
+  
+  ## Installation
+  
+  Installation Instructions: ${install}
+  
+  ## Usage
+  This repository can be viewed at the following link: (link)
+  
+  It should look like the following:
+  
+  ![Screenshot](./screenshot.PNG)
+  
+  How to use the program:
+  \`\`\`
+  ${usage}
+  \`\`\`
+      
+  ## Contributing
+  
+  ${contribute}
+  
+  ## Tests
+  
+  ${test}
+
+  ## Credits
+  
+  ${credit}
+  
+  ## License
+  
+  ${userlicense}`;
+}
+
+module.exports = generateMarkdown;
